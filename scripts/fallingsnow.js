8bd3c1423a4d404f070d9a7651c757c2feb7a0eb
Elaine Broussard <elaine@elainebroussard.com>
2015-08-22 19:55:03 -0500

    Add project.

---
+++ scripts/fallingsnow.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#snowy').autoBackgroundScroll({
+		speed: 0.3,
+		direction1: 'bottom',
+		direction2: 'right',
+		imageWidth: 720,
+		imageHeight: 720
+	});
+});
